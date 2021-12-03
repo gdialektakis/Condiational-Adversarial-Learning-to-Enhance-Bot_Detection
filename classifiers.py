daec9a0ce059164fe7e5fd66d27bd7a860e37314
George Dialektakis <geo4diale@gmail.com>
2021-12-03 12:40:29 +0200

    Created several methods for our experiments

--- classifiers.py
+++ classifiers.py
@@ -10,10 +10,12 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.ensemble import VotingClassifier
-from sklearn.model_selection import StratifiedKFold
 from sklearn.ensemble import BaggingClassifier, AdaBoostClassifier
 from imblearn.over_sampling import SMOTE, ADASYN
+from imblearn.metrics import geometric_mean_score
 import warnings
+
+warnings.filterwarnings("ignore", category=Warning)
 
 
 def run_classifiers(X_train, X_test, y_train, y_test):
@@ -41,6 +43,7 @@ def run_classifiers(X_train, X_test, y_train, y_test):
         start_time = time.time()
         classifier.fit(X_train, y_train)
         print("\n--- Time taken %s seconds ---\n" % (time.time() - start_time))
+
         # Plotting the learning curve for each classifier
         # Create the learning curve visualizer
         """
@@ -54,7 +57,6 @@ def run_classifiers(X_train, X_test, y_train, y_test):
         filename = 'pretrained_models/%s_pretrained.sav' % classifier_name
         # pickle.dump(classifier, open(filename, 'wb'))
 
-        y_pred_train = classifier.predict(X_train)
         y_pred = classifier.predict(X_test)
 
         print("==============================")
@@ -63,10 +65,11 @@ def run_classifiers(X_train, X_test, y_train, y_test):
         print("Precision: ", metrics.precision_score(y_test, y_pred, average='macro'))
         print("F1-score: ", metrics.f1_score(y_test, y_pred, average='macro'))
         print("Recall-score: ", metrics.recall_score(y_test, y_pred, average='macro'))
+        print("G-Mean: ", geometric_mean_score(y_test, y_pred, correction=0.0001))
         print("==============================\n")
 
 
-def train_with_original_data():
+def train_and_test_on_original_data():
     train_data = pickle.load(open('data/train_multiclass_data', 'rb'))
     test_data = pickle.load(open('data/test_multiclass_data', 'rb'))
 
@@ -100,12 +103,80 @@ def train_with_original_data():
     run_classifiers(X_smote, X_test, y_smote, y_test)
 
 
-def train_with_augmented_data():
-    print('\n---------------- Training with balanced synthetic samples for each class -------------------\n')
+def train_on_original_and_test_on_synthetic_data():
     df = pickle.load(open('data/train_multiclass_data', 'rb'))
-    # synthetic_data = pickle.load(open('data/synthetic_data/conditional_gan_multiclass/synthetic_data_30000_per_class', 'rb'))
     synthetic_data = pickle.load(
         open('data/synthetic_data/conditional_gan_multiclass/synthetic_data_balanced_per_class', 'rb'))
+    synthetic_data = synthetic_data.sample(frac=1)
+
+    y_train = df['label']
+    y_test = synthetic_data['label']
+    print(df['label'].value_counts())
+    print(synthetic_data['label'].value_counts())
+
+    # Drop unwanted columns
+    df = df.drop(['label'], axis=1)
+    synthetic_data = synthetic_data.drop(['label'], axis=1)
+
+    # split the labeled tweets into train and test set
+    X_train = df
+    X_test = synthetic_data
+
+    # Scale our data in the range of (0, 1)
+    scaler = MinMaxScaler()
+    X_train = scaler.fit_transform(X=X_train)
+    X_test = scaler.transform(X=X_test)
+
+    y_train = y_train.astype('int')
+    y_test = y_test.astype('int')
+
+    print('\n~~~~~~~~ Running Classifiers ~~~~~~~~~~~~~~~')
+    run_classifiers(X_train, X_test, y_train, y_test)
+
+
+def train_on_original_and_test_on_augmented_data():
+    print('\n ~~~~~~~~~~~ Train on Original and Test on Augmented Data ~~~~~~~~~~~~~\n')
+
+    df = pickle.load(open('data/train_multiclass_data', 'rb'))
+    synthetic_data_balanced = pickle.load(
+        open('data/synthetic_data/conditional_gan_multiclass/synthetic_data_balanced_per_class', 'rb'))
+
+    original_test_data = pickle.load(open('data/test_multiclass_data', 'rb'))
+
+    synthetic_test_data = synthetic_data_balanced.sample(n=13000)
+    test_data = synthetic_test_data.append(original_test_data)
+    test_data = test_data.sample(frac=1)
+
+    y_train = df['label']
+    y_test = test_data['label']
+
+    # Drop label column
+    df = df.drop(['label'], axis=1)
+    test_data = test_data.drop(['label'], axis=1)
+
+    # split the labeled tweets into train and test set
+    X_train = df
+    X_test = test_data
+
+    # Scale our data in the range of (0, 1)
+    scaler = MinMaxScaler()
+    X_train = scaler.fit_transform(X=X_train)
+    X_test = scaler.transform(X=X_test)
+
+    y_train = y_train.astype('int')
+    y_test = y_test.astype('int')
+
+    print('\n~~~~~~~~ Running Classifiers ~~~~~~~~~~~~~~~')
+    run_classifiers(X_train, X_test, y_train, y_test)
+
+
+def train_on_augmented_and_test_on_original_data():
+    print('\n ~~~~~~~~~~~~~~~ Train with Augmented Data and Test on Original ~~~~~~~~~~~~~~~~')
+    print('\n---------------- Training with 30000 new synthetic samples for each class  -------------------')
+    df = pickle.load(open('data/train_multiclass_data', 'rb'))
+    synthetic_data = pickle.load(
+        open('data/synthetic_data/conditional_gan_multiclass/synthetic_data_30000_per_class', 'rb'))
+    # synthetic_data = pickle.load(open('data/synthetic_data/conditional_gan_multiclass/synthetic_data_balanced_per_class', 'rb'))
     synthetic_data = synthetic_data.sample(frac=1)
     augmented_df = df.append(synthetic_data)
     augmented_df = augmented_df.sample(frac=1)
@@ -143,34 +214,34 @@ def train_with_augmented_data():
     run_classifiers(X_train, X_test, y_train, y_test)
 
 
-# TODO
-def train_with_original_and_test_on_synthetic_data():
-    df = pickle.load(open('data/multi_class_data', 'rb'))
-    synthetic_data = pickle.load(open('gans/conditional_gan_multi/synthetic_multiclass_data_30000_each_class', 'rb'))
-    synthetic_data = synthetic_data.sample(frac=1)
-    # Convert features that are boolean to integers
-    df = df.applymap(lambda x: int(x) if isinstance(x, bool) else x)
+def train_on_augmented_and_test_on_synthetic_data():
+    print('\n ~~~~~~~~~~~ Train on Augmented and Testing on Synthetic Data ~~~~~~~~~~~~~\n')
 
-    y = df['label']
-    print(df['label'].value_counts())
+    df = pickle.load(open('data/train_multiclass_data', 'rb'))
+    synthetic_data_30K = pickle.load(
+        open('data/synthetic_data/conditional_gan_multiclass/synthetic_data_30000_per_class', 'rb'))
+    synthetic_data_balanced = pickle.load(
+        open('data/synthetic_data/conditional_gan_multiclass/synthetic_data_balanced_per_class', 'rb'))
 
-    # Drop unwanted columns
-    df = df.drop(['user_name', 'user_screen_name', 'user_id', 'label'], axis=1)
-    if 'max_appearance_of_punc_mark' in df.columns:
-        df = df.drop(['max_appearance_of_punc_mark'], axis=1)
+    synthetic_data_train = synthetic_data_30K.sample(frac=1)
+
+    augmented_df = df.append(synthetic_data_train)
+    augmented_df = augmented_df.sample(frac=1)
+
+    test_data = synthetic_data_balanced.sample(frac=1)
+
+    y_train = augmented_df['label']
+    y_test = test_data['label']
+    print(augmented_df['label'].value_counts())
+    print(test_data['label'].value_counts())
+
+    # Drop label column
+    augmented_df = augmented_df.drop(['label'], axis=1)
+    test_data = test_data.drop(['label'], axis=1)
 
     # split the labeled tweets into train and test set
-    X_train = df
-    y_train = y
-
-    # Convert features that are boolean to integers
-    synthetic_data = synthetic_data.applymap(lambda x: int(x) if isinstance(x, bool) else x)
-
-    y_test = synthetic_data['label']
-
-    # Drop unwanted columns
-    synthetic_data = synthetic_data.drop(['label'], axis=1)
-    X_test = synthetic_data
+    X_train = augmented_df
+    X_test = test_data
 
     # Scale our data in the range of (0, 1)
     scaler = MinMaxScaler()
@@ -180,21 +251,60 @@ def train_with_original_and_test_on_synthetic_data():
     y_train = y_train.astype('int')
     y_test = y_test.astype('int')
 
-    # SMOTE
-    smote_sampler = SMOTE(sampling_strategy='not majority', n_jobs=-1)
-    X_smote, y_smote = smote_sampler.fit_resample(X_train, y_train)
+    print('\n~~~~~~~~ Running Classifiers ~~~~~~~~~~~~~~~')
+    run_classifiers(X_train, X_test, y_train, y_test)
 
-    # summarize class distribution
-    print("\nLabel distribution before SMOTE: {}".format(Counter(y_train)))
-    print("Label distribution after SMOTE: {}\n".format(Counter(y_smote)))
+
+def train_and_test_on_augmented_data():
+    print('\n ~~~~~~~~~~~ Train on Augmented and Testing on Augmented Data ~~~~~~~~~~~~~\n')
+
+    df = pickle.load(open('data/train_multiclass_data', 'rb'))
+    synthetic_data_30K = pickle.load(
+        open('data/synthetic_data/conditional_gan_multiclass/synthetic_data_30000_per_class', 'rb'))
+    synthetic_data_balanced = pickle.load(
+        open('data/synthetic_data/conditional_gan_multiclass/synthetic_data_balanced_per_class', 'rb'))
+
+    synthetic_data_train = synthetic_data_30K.sample(frac=1)
+    augmented_df = df.append(synthetic_data_train)
+    augmented_df = augmented_df.sample(frac=1)
+
+    original_test_data = pickle.load(open('data/test_multiclass_data', 'rb'))
+    print(original_test_data['label'].value_counts())
+    synthetic_test_data = synthetic_data_balanced.sample(n=13000)
+    test_data = synthetic_test_data.append(original_test_data)
+    test_data = test_data.sample(frac=1)
+
+    y_train = augmented_df['label']
+    y_test = test_data['label']
+    print(augmented_df['label'].value_counts())
+    print(original_test_data['label'].value_counts())
+
+    # Drop label column
+    augmented_df = augmented_df.drop(['label'], axis=1)
+    test_data = test_data.drop(['label'], axis=1)
+
+    # split the labeled tweets into train and test set
+    X_train = augmented_df
+    X_test = test_data
+
+    # Scale our data in the range of (0, 1)
+    scaler = MinMaxScaler()
+    X_train = scaler.fit_transform(X=X_train)
+    X_test = scaler.transform(X=X_test)
+
+    y_train = y_train.astype('int')
+    y_test = y_test.astype('int')
 
     print('\n~~~~~~~~ Running Classifiers ~~~~~~~~~~~~~~~')
     run_classifiers(X_train, X_test, y_train, y_test)
 
-    print('\n~~~~~~~~ Running Classifiers with SMOTE ~~~~~~~~~~~~~~~')
-    run_classifiers(X_smote, X_test, y_smote, y_test)
 
+# Train on Augmented Data
+# train_and_test_on_augmented_data()
+# train_on_augmented_and_test_on_original_data()
+# train_on_augmented_and_test_on_synthetic_data()
 
-train_with_original_data()
-# train_with_original_and_test_on_synthetic_data()
-#train_with_augmented_data()
+# Train on Original Data
+# train_and_test_on_original_data()
+train_on_original_and_test_on_augmented_data()
+# train_on_original_and_test_on_synthetic_data()
